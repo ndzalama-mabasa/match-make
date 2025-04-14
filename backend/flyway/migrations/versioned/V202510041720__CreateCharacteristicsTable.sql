af34e690385cf14186f9cf6c91e1be28575107a6
Temwa-BBD <temwa.nyirenda@vcontractor.co.za>
2025-04-14 20:50:10 +0200

    Fixed bugs

--- backend/flyway/migrations/versioned/V202510041720__CreateCharacteristicsTable.sql
+++ backend/flyway/migrations/versioned/V202510041720__CreateCharacteristicsTable.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS characteristics (
    id SERIAL PRIMARY KEY NOT NULL,
-   characteristic_categories_id VARCHAR NOT NULL,
+   characteristic_categories_id SERIAL NOT NULL,
    characteristics_name VARCHAR NOT NULL,
    CONSTRAINT fk_characteristics_characteristic_categories FOREIGN KEY (characteristic_categories_id) REFERENCES characteristic_categories(id)
 );
--- backend/flyway/migrations/versioned/V202510041722__CreateProfilePreferencesTable.sql
+++ backend/flyway/migrations/versioned/V202510041722__CreateProfilePreferencesTable.sql
@@ -2,6 +2,6 @@ CREATE TABLE IF NOT EXISTS  profile_preferences (
     id SERIAL PRIMARY KEY NOT NULL,
     profile_id SERIAL NOT NULL,
     characteristic_id SERIAL NOT NULL,
-    CONSTRAINT fk_profile_preferences_profiles FOREIGN KEY (profile_id_id) REFERENCES profiles(id),
-    CONSTRAINT fk_profile_preferences_characteristic FOREIGN KEY (characteristic_id_id) REFERENCES characteristics(id)
+    CONSTRAINT fk_profile_preferences_profiles FOREIGN KEY (profile_id) REFERENCES profiles(id),
+    CONSTRAINT fk_profile_preferences_characteristic FOREIGN KEY (characteristic_id) REFERENCES characteristics(id)
 );
