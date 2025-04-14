addf15b440747c54255ba71b5fa6c29d4b4fdbe7
Temwa-BBD <temwa.nyirenda@vcontractor.co.za>
2025-04-14 16:37:57 +0200

    Fixed foreign keys

--- backend/flyway/migrations/versioned/V202510041720__CreateProfileAttributesTable.sql
+++ backend/flyway/migrations/versioned/V202510041720__CreateProfileAttributesTable.sql
@@ -2,6 +2,6 @@ CREATE TABLE profile_attributes (
     id SERIAL PRIMARY KEY NOT NULL,
     profile_id SERIAL NOT NULL,
     characteristic_id SERIAL NOT NULL,
-    CONSTRAINT fk_profile_attributes_profiles FOREIGN KEY (interest_id) REFERENCES profiles(id),
-    CONSTRAINT fk_profile_attributes_characteristic FOREIGN KEY (user_id) REFERENCES characteristics(id)
+    CONSTRAINT fk_profile_attributes_profiles FOREIGN KEY (profile_id) REFERENCES profiles(id),
+    CONSTRAINT fk_profile_attributes_characteristic FOREIGN KEY (characteristic_id) REFERENCES characteristics(id)
 );
--- backend/flyway/migrations/versioned/V202510041720__CreateProfilePreferencesTable.sql
+++ backend/flyway/migrations/versioned/V202510041720__CreateProfilePreferencesTable.sql
@@ -2,6 +2,6 @@ CREATE TABLE profile_preferences (
     id SERIAL PRIMARY KEY NOT NULL,
     profile_id SERIAL NOT NULL,
     characteristic_id SERIAL NOT NULL,
-    CONSTRAINT fk_profile_preferences_profiles FOREIGN KEY (interest_id) REFERENCES profiles(id),
-    CONSTRAINT fk_profile_preferences_characteristic FOREIGN KEY (user_id) REFERENCES characteristics(id)
+    CONSTRAINT fk_profile_preferences_profiles FOREIGN KEY (profile_id_id) REFERENCES profiles(id),
+    CONSTRAINT fk_profile_preferences_characteristic FOREIGN KEY (characteristic_id_id) REFERENCES characteristics(id)
 );
